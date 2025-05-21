4c2699d438484a164419a59664a7a63c84aef314
Trying-1 <tryinbait@gmail.com>
2025-05-21 10:16:07 +0530

    meta tag added app/layout.tsx for website ownership verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <meta name="google-adsense-account" content="ca-pub-5829296907403264" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5829296907403264"
